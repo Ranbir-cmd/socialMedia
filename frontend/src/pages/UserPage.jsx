3f0bc9b71cb33c0010d17c49ceb2c17e7851bc73
uplRanabir <ranabir.mandal@utplco.com>
2024-04-14 16:36:05 +0530

    spinner issue

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -35,7 +35,7 @@ const UserPage = () => {
     if(!user && loading){
         return (
             <Flex justifyContent={"center"}>
-                <Spinner size={"xl"} />;
+                <Spinner size={"xl"} />
             </Flex>
         )
     } 

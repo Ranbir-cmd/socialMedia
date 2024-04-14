99401a09402ac671989f43ea5c7fd0e776d415d6
uplRanabir <ranabir.mandal@utplco.com>
2024-04-14 17:02:21 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const __dirname = path.resolve()
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    api_secret: "-" + process.env.CLOUDINARY_API_SECRET
 })
 
 import userRoutes from "./routes/user.routes.js"
